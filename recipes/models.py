58c14162a83e40ce3b52da20a930802817145aee
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-02 17:03:17 -0300

    QuerySet

--- recipes/models.py
+++ recipes/models.py
@@ -22,7 +22,8 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
@@ -30,6 +31,5 @@ class Recipe(models.Model):
         User, on_delete=models.SET_NULL, null=True
     )
 
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
