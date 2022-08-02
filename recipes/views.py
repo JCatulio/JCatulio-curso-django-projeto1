c80d38600f762f797325b8c78e40d2becb91a4de
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-02 11:39:56 -0300

    nova view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Luiz Otávio',
     })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
