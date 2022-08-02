82e595ceac5a79b25a7cd28abbb47ad24f265089
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-02 15:23:33 -0300

    category admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,9 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
