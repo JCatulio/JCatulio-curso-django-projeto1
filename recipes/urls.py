43901e87ff1802903338fc1c4243faaf86cfa253
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-03 18:15:32 -0300

    URL search TDD

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-# recipes:recipe
 app_name = 'recipes'
 
 urlpatterns = [
@@ -10,4 +9,5 @@ urlpatterns = [
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
     path('recipes/<int:id>/', views.recipes, name="recipe"),
+    path('recipes/<int:id>/', lambda request:..., name="search"),
 ]
