452411086fe6e86477e778bd11f634592b20f165
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-04 18:40:04 -0300

    ajustando css

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 import os
 
 from django.db.models import Q
-from django.http import Http404
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
 
