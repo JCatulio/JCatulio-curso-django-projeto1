23b94a09d884361763b53306ff3ee024fd218672
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-07 23:06:40 -0300

    Corrigindo os testes

--- recipes/templates/recipes/tests/test_recipe_detail_view.py
+++ recipes/templates/recipes/tests/test_recipe_detail_view.py
@@ -1,17 +1,16 @@
 from django.urls import resolve, reverse
 from recipes import views
-
-from .test_recipe_base import RecipeTestBase
+from recipes.templates.recipes.tests.test_recipe_base import RecipeTestBase
 
 
 class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipes)
+        view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
+            reverse('recipes:recipe', kwargs={'pk': 1000})
         )
         self.assertEqual(response.status_code, 404)
 
@@ -22,7 +21,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         response = self.client.get(
             reverse(
                 'recipes:recipe',
-                kwargs={'id': 1}
+                kwargs={'pk': 1}
             )
         )
         content = response.content.decode('utf-8')
@@ -36,7 +35,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         response = self.client.get(
             reverse(
                 'recipes:recipe',
-                kwargs={'id': recipe.id}
+                kwargs={'pk': recipe.id}
             )
         )
 
