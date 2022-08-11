e6e4333951d2079b1c5bb80907926c9a4b23db48
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-08-11 14:30:41 -0300

    Criando a lógica de logout

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -12,31 +12,43 @@
   }
 })();
 
-  (() => {
-    const buttonCloseMenu = document.querySelector('.button-close-menu');
-    const buttonShowMenu = document.querySelector('.button-show-menu');
-    const menuContainer = document.querySelector('.menu-container');
+(() => {
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
 
-    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
-    const menuHiddenClass = 'menu-hidden';
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
 
-    const closeMenu = () => {
-      buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
-      menuContainer.classList.add(menuHiddenClass);
-    };
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
 
-    const showMenu = () => {
-      buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
-      menuContainer.classList.remove(menuHiddenClass);
-    };
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
 
-    if (buttonCloseMenu) {
-      buttonCloseMenu.removeEventListener('click', closeMenu);
-      buttonCloseMenu.addEventListener('click', closeMenu);
-    }
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  }
 
-    if (buttonShowMenu) {
-      buttonShowMenu.removeEventListener('click', showMenu);
-      buttonShowMenu.addEventListener('click', showMenu);
-    }
-  })();
+  if (buttonShowMenu) {
+    buttonShowMenu.removeEventListener('click', showMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  }
+})();
+
+(() => {
+  const authorsLogoutLinks = document.querySelectorAll('.author-logout-link');
+  const formLogout = document.querySelector('.form-logout');
+
+  for (const link of authorsLogoutLinks) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      formLogout.submit();
+    });
+  }
+})();
